b71d7afbee99c1093ac7b9ce27527ba76559b2b7
Darshan V <reachdarshanv@gmail.com>
2022-01-12 22:26:41 +0530

    modified server file

--- server.js
+++ server.js
@@ -88,7 +88,10 @@ app.post("/api/addtask",(req,res)=>{
 })
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("client/build"));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
 }
 
 app.listen(PORT,() =>{
