080c02860eb886af3744ecb8df9120e082f09f1e
Darshan V <reachdarshanv@gmail.com>
2022-01-13 14:49:47 +0530

    tried to add responsiveness

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,19 +17,18 @@ const Front = () => {
             </div>
             <hr style={{backgroundColor:"white",height:"4px"}}></hr>
             <div >
-                <h2 style={{font:"message box",textAlign:"center",fontFamily:"Brush Script MT",fontSize:"8vh"}}>
+                <h2 style={{font:"message box",textAlign:"center",fontFamily:"Brush Script MT"}}>
                 MY TODO APP
                 </h2>
             </div>
             <hr style={{backgroundColor:"white",height:"2px"}}></hr>
-            <div style={{display:"flex"}}>
-                <img style={{height:"80vh",width:"60%"}} src="https://i.ibb.co/mJSCJ5z/todoo.jpg" alt="todo quote"/>
-                    <div style={{display:"grid",height:"30px",alignContent:"center",border:"3px solid coral",borderStyle:"solid",
-                    padding:"6vw",position:"relative",overflow:"none"}}>
-                        <h3 style={{fontSize:"3vh"}}>ANY SUGGESTIONS?</h3>
-                        <h5 style={{fontSize:"3vh"}}>REACH ME  
-                     <Mailto email="reachdarshanv@gmail.com" obfuscate={true}>
-                         @DARSHAN V
+            <div style={{display:"flex"}} className="ti">
+                <img className="lp" style={{height:"80%",width:"50%"}} src="https://i.ibb.co/mJSCJ5z/todoo.jpg" alt="todo quote"/>
+                    <div style={{display:"grid",height:"20vh",alignContent:"center",border:" solid coral",borderStyle:"solid",paddingTop:"10px",
+                   position:"relative",overflow:"none"}} className="ll">
+                        <h3 style={{fontSize:"3vw"}}>ANY SUGGESTIONS?</h3>
+                        <h5 style={{fontSize:"3vh",paddingBottom:"0vh"}}>mail  
+                     <Mailto  email="reachdarshanv@gmail.com" obfuscate={true}>@DARSHAN_V
                      </Mailto>
                         </h5>
                     </div>
